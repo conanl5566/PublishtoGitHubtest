164e00a973c43a0edea7321bbe43a26f30665a9c
conanl <conanl@CONANL-PC>
2019-12-06 10:12:55 +0800

    添加项目文件。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PublishtoGitHubtest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
